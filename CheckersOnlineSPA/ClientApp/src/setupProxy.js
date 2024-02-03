72c757202af00a1ffa4f614a794f88d3d19b38f4
Vitaliy Novak <vitaliynovak555@gmail.com>
2024-02-03 02:31:30 +0200

    Hate it

--- CheckersOnlineSPA/ClientApp/src/setupProxy.js
+++ CheckersOnlineSPA/ClientApp/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const httpProxy = require('http-proxy');
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+const target = env.ASPNETCORE_HTTPS_PORT ? `http://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5015';
 
 const webSocketTarget = env.ASPNETCORE_HTTPS_PORT ? `ws://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
